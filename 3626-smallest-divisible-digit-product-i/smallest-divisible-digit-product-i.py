ff612857280c19f6b41c83a7b2f826997e9b77dd
saikarthickgit <126444033+saikarthickgit@users.noreply.github.com>
2026-08-06 21:03:16 -0400

    Time: 0 ms (100.00%) | Memory: 19.3 MB (31.87%) - LeetSync

--- 3626-smallest-divisible-digit-product-i/smallest-divisible-digit-product-i.py
+++ 3626-smallest-divisible-digit-product-i/smallest-divisible-digit-product-i.py
@@ -1,14 +1,15 @@
 class Solution:
-    def prodDigits(self, n:int) -> int:
-        prod = 1
-        while n>0:
-            prod *= n%10
-            n //= 10
-        return prod
-
     def smallestNumber(self, n: int, t: int) -> int:
-        while True:
-            if self.prodDigits(n)%t == 0:
-                return n
-            else:
-                n+=1
+
+        def repeat_it(n):
+            sum=1
+            while n>0:
+                sum*=n%10
+                n=n//10
+            return sum
+
+        while repeat_it(n)%t!=0:
+            n+=1
+        return n
+
+        
