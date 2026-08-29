e6346183c14cc96a16328865bb89b7c43831b574
saikarthickgit <126444033+saikarthickgit@users.noreply.github.com>
2026-08-29 17:14:08 -0400

    Time: 3 ms (92.91%) | Memory: 22.4 MB (88.71%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.py
+++ 229-majority-element-ii/majority-element-ii.py
@@ -1,16 +1,29 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        temp=[]
-        n=len(nums)
-        t=n//3
-        c=Counter(nums)
-        for i, j in c.items():
-            if j >t:
-                temp.append(i)
-        return temp
+        ans=[]
+        cand1, cand2 = None, None
+        count1, count2 = 0, 0
+        for num in nums:
+            if num == cand1:
+                count1 += 1
+            elif num == cand2:
+                count2 += 1
+            elif count1 == 0:
+                cand1, count1 = num, 1
+            elif count2 == 0:
+                cand2, count2 = num, 1
+            else:
+                count1 -= 1
+                count2 -= 1
 
+     
+        threshold = len(nums) // 3
 
-        
-        
+        if cand1 is not None and nums.count(cand1) > threshold:
+            ans.append(cand1)
 
+        if cand2 is not None and nums.count(cand2) > threshold:
+            ans.append(cand2)
+
+        return ans
         
