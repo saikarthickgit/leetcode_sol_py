32b4e640581f5cef6b4c9245c7dbb5194981a066
saikarthickgit <126444033+saikarthickgit@users.noreply.github.com>
2026-09-06 10:41:04 -0400

    Time: 0 ms (100.00%) | Memory: 19.4 MB (17.77%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -6,7 +6,7 @@ class Solution:
         prev2 = 1  # ways to reach step 1
         prev1 = 2  # ways to reach step 2
 
-        for _ in range(3, n + 1):
+        for i in range(3, n + 1):
             curr = prev1 + prev2
             prev2 = prev1
             prev1 = curr
